615348d996aaf8b22d33f0bc4d4868bf5067a69e
vijaypvk <vijaypvk001@gmail.com>
2026-07-15 23:54:33 +0530

    change in application file

--- cicdapp/index.js
+++ cicdapp/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("CI/CD with Jenkins + Argo CD 🚀");
+  res.send("CI/CD Pipeline with Git, Jenkins, Docker, AWS ECR & Kubernetes 🚀");
 });
 
 app.listen(port, () => {
